0d513f87f3145d67360cbf25344963f05eaf16b9
Lucas Veras <lucasdsveras@gmail.com>
2019-12-09 10:50:35 +0000

    Remove function definition from data analysis script

--- code/scripts/analysis.R
+++ code/scripts/analysis.R
@@ -8,9 +8,8 @@ library(emmeans)
 
 # Load and prepare data ---------------------------------------------------
 
-# data <- read_csv("data/database.csv")
-data <- read_csv("data/Base.csv") # new data base
-data <- data %>% select(Subj, Time, Group, LS__BMD, LS__BMD_adjust)
+data <- read_csv("data/database.csv")
+data <- data %>% select(subj, time, group, LS_BMD, LS_BMD_adjust)
 
 # Code the group and time variables as factors
 data$Group <- as.factor(data$Group)
@@ -22,78 +21,6 @@ contrasts(data$Time) <- contr.poly(4)
 # Set contrasts of variable Group to deviation
 contrasts(data$Group) <- matrix(rev(contr.sum(2)), ncol = 1)
 
-# Create a function to center a variable
-center_variable <- function(data, variable) {
-  # Center a variable based on the group means
-  #
-  # Args:
-  #   data: A data frame containing the variable to be centered and a Group
-  #   factor
-  #   variable: The name of the variable to be centered as character string
-  #
-  # Returns:
-  #   The original data frame plus a column in the last position with the
-  #   centered variable
-  
-  data  <- as.data.frame(data)
-  n_col <- ncol(data)
-  idx   <- which(names(data) == variable)
-  
-  # Control group
-  CG <- data %>% filter(Group == 0)
-  CG_mean <- mean(CG[, idx], na.rm = TRUE)
-  CG_centered <- NA
-  for (i in 1:nrow(CG)) {
-    CG_centered[i] <- CG[i, variable] - CG_mean 
-  }
-  CG <- cbind(CG, CG_centered)
-  
-  # Exercise group
-  EG <- data %>% filter(Group == 1)
-  EG_mean <- mean(EG[, idx], na.rm = TRUE)
-  EG_centered <- NA
-  for (i in 1:nrow(EG)) {
-    EG_centered[i] <- EG[i, variable] - EG_mean 
-  }
-  EG <- cbind(EG, EG_centered)
-  
-  # NA group
-  NG <- data %>% filter(is.na(Group))
-  NG$NG_centered <- NA
-  
-  # Join data frames
-  data <- plyr::join_all(
-    list(CG, EG, NG),
-    type = "full",
-    by = names(data)
-  )
-  data <- as_tibble(data)
-  
-  # Keep only one column for the centered variable
-  data$centered <- NA
-  for (i in 1:nrow(data)) {
-    if (is.na(data$EG_centered[i]) & is.na(data$NG_centered[i])) {
-      data$centered[i] <- data$CG_centered[i]
-    } else {
-      if (is.na(data$CG_centered[i]) & is.na(data$NG_centered[i])) {
-        data$centered[i] <- data$EG_centered[i]
-      } else {
-        if (is.na(data$EG_centered[i]) & is.na(data$CG_centered[i])) {
-          data$centered[i] <- data$NG_centered
-        }
-      }
-    }
-  }
-  data <- data[, c(1:n_col, ncol(data))]
-  
-  # Rename centered variable
-  names(data)[ncol(data)] <- str_c(variable, "_centered", sep = "")
-  
-  # Arrange dataframe before returning
-  data <- arrange(data, Subj, Time)
-  
-  return(data)
-}
 # Center variable
 data <- center_variable(data, "LS__BMD_adjust")
 
