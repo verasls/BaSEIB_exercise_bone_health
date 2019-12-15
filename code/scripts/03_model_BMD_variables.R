b614a4547a878b77c1f875da55806b32a815211b
Lucas Veras <lucasdsveras@gmail.com>
2019-12-15 21:23:59 +0000

    Reorder analysis

--- code/scripts/03_model_BMD_variables.R
+++ code/scripts/03_model_BMD_variables.R
@@ -17,9 +17,9 @@ contrasts(df$group) <- matrix(rev(contr.sum(2)), ncol = 1)
 contrasts(df$time) <- contr.poly(4)
 
 # Select variables
-LS_data <- df %>% select(subj, time, group, LS_BMD, LS_BMD_adjust)
 TH_data <- df %>% select(subj, time, group, TH_BMD, TH_BMD_adjust)
 FN_data <- df %>% select(subj, time, group, FN_BMD, FN_BMD_adjust)
+LS_data <- df %>% select(subj, time, group, LS_BMD, LS_BMD_adjust)
 TR_data <- df %>% select(subj, time, group, TR_BMD, TR_BMD_adjust)
 
 # Center variables
@@ -29,38 +29,6 @@ FN_data <- center_variable(FN_data, "FN_BMD_adjust")
 TR_data <- center_variable(TR_data, "TR_BMD_adjust")
 
 # Build models ------------------------------------------------------------
-
-# ** LS_BMD ---------------------------------------------------------------
-
-LS_LMM <- lmer(
-  formula = LS_BMD ~ 1 + group + time + group:time + LS_BMD_adjust_centered + (1 | subj),
-  data = LS_data
-)
-
-# R-squared
-rsquared(LS_LMM)
-
-# Fixed effects test
-anova(LS_LMM, type = 3, test = "F")
-
-# Random components and fixed effects parameters estimates
-summary(LS_LMM)
-
-# Estimated marginal means for group
-group_LS_emm <- emmeans(LS_LMM, ~ group)
-
-# Estimated marginal means for time
-time_LS_emm <- emmeans(LS_LMM, ~ time)
-
-# Estimated marginal means for group x time interaction
-interaction_LS_emm  <- emmeans(LS_LMM, ~ group:time)
-# Save into a data frame to build the plots
-interaction_LS_emm_df <- as.data.frame(interaction_LS_emm)
-write_csv(interaction_LS_emm_df, "output/interaction_LS_emm.csv")
-
-# Post hocs
-ph_LS_none <- pairs(interaction_LS_emm, adjust = "none")
-ph_LS_bonf <- bonferroni(as.data.frame(ph_LS_none), 16)
 
 # ** TH_BMD ---------------------------------------------------------------
 
@@ -125,6 +93,38 @@ write_csv(interaction_FN_emm_df, "output/interaction_FN_emm.csv")
 # Post hocs
 ph_FN_none <- pairs(interaction_FN_emm, adjust = "none")
 ph_FN_bonf <- bonferroni(as.data.frame(ph_FN_none), 16)
+
+# ** LS_BMD ---------------------------------------------------------------
+
+LS_LMM <- lmer(
+  formula = LS_BMD ~ 1 + group + time + group:time + LS_BMD_adjust_centered + (1 | subj),
+  data = LS_data
+)
+
+# R-squared
+rsquared(LS_LMM)
+
+# Fixed effects test
+anova(LS_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(LS_LMM)
+
+# Estimated marginal means for group
+group_LS_emm <- emmeans(LS_LMM, ~ group)
+
+# Estimated marginal means for time
+time_LS_emm <- emmeans(LS_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_LS_emm  <- emmeans(LS_LMM, ~ group:time)
+# Save into a data frame to build the plots
+interaction_LS_emm_df <- as.data.frame(interaction_LS_emm)
+write_csv(interaction_LS_emm_df, "output/interaction_LS_emm.csv")
+
+# Post hocs
+ph_LS_none <- pairs(interaction_LS_emm, adjust = "none")
+ph_LS_bonf <- bonferroni(as.data.frame(ph_LS_none), 16)
 
 # ** TR_BMD ---------------------------------------------------------------
   
