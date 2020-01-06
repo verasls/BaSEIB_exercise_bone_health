6684aaad41dd6e04216246405b5be59c5c4c925c
Lucas Veras <lucasdsveras@gmail.com>
2020-01-06 14:19:58 +0000

    Initial commit

---
+++ code/scripts/06_model_delta_biochemical_BMSi_variables.R
@@ -0,0 +1,201 @@
+# Load packages and functions ---------------------------------------------
+
+library(tidyverse)
+library(lme4)
+library(lmerTest)
+library(piecewiseSEM)
+library(emmeans)
+source("code/functions/center_variable.R")
+source("code/functions/bonferroni.R")
+
+# Load and prepare data ---------------------------------------------------
+
+source("code/scripts/01_tidy_data.R")
+# Set contrasts of variable group to deviation
+contrasts(df$attend_cat) <- matrix(rev(contr.sum(3)), ncol = 2)
+# Set contrasts of variable time to polynomial
+contrasts(df$time) <- contr.poly(4)
+
+# Select variables
+P1NP_delta_data       <- df %>% select(subj, time, attend_cat, delta_P1NP, BMI_adjust)
+CTX_delta_data        <- df %>% select(subj, time, attend_cat, delta_CTX, BMI_adjust)
+PTH_delta_data        <- df %>% select(subj, time, attend_cat, delta_PTH, BMI_adjust)
+vitD_delta_data       <- df %>% select(subj, time, attend_cat, delta_vitD, BMI_adjust)
+sclerostin_delta_data <- df %>% select(subj, time, attend_cat, delta_sclerostin, BMI_adjust)
+BMSi_delta_data       <- df %>% select(subj, time, attend_cat, delta_BMSi, BMI_adjust)
+
+# Build models ------------------------------------------------------------
+
+# ** delta_P1NP -----------------------------------------------------------
+
+delta_P1NP_LMM <- lmer(
+  formula = delta_P1NP ~ 1 + attend_cat + time + attend_cat:time + BMI_adjust + (1 | subj),
+  data = P1NP_delta_data
+)
+
+# R-squared
+rsquared(delta_P1NP_LMM)
+
+# Fixed effects test
+anova(delta_P1NP_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(delta_P1NP_LMM)
+
+# Estimated marginal means for group
+group_delta_TH_emm <- emmeans(delta_P1NP_LMM, ~ attend_cat)
+
+# Estimated marginal means for time
+time_delta_TH_emm <- emmeans(delta_P1NP_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_delta_P1NP_emm  <- emmeans(delta_P1NP_LMM, ~ attend_cat:time)
+
+# Post hocs
+ph_delta_P1NP_none <- pairs(interaction_delta_P1NP_emm, adjust = "none")
+ph_delta_P1NP_bonf <- bonferroni(as.data.frame(ph_delta_P1NP_none), 3)
+
+# ** delta_CTX ------------------------------------------------------------
+
+delta_CTX_LMM <- lmer(
+  formula = delta_CTX ~ 1 + attend_cat + time + attend_cat:time + BMI_adjust + (1 | subj),
+  data = CTX_delta_data
+)
+
+# R-squared
+rsquared(delta_CTX_LMM)
+
+# Fixed effects test
+anova(delta_CTX_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(delta_CTX_LMM)
+
+# Estimated marginal means for group
+group_delta_TH_emm <- emmeans(delta_CTX_LMM, ~ attend_cat)
+
+# Estimated marginal means for time
+time_delta_TH_emm <- emmeans(delta_CTX_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_delta_CTX_emm  <- emmeans(delta_CTX_LMM, ~ attend_cat:time)
+
+# Post hocs
+ph_delta_CTX_none <- pairs(interaction_delta_CTX_emm, adjust = "none")
+ph_delta_CTX_bonf <- bonferroni(as.data.frame(ph_delta_CTX_none), 3)
+
+# ** delta_PTH ------------------------------------------------------------
+
+delta_PTH_LMM <- lmer(
+  formula = delta_PTH ~ 1 + attend_cat + time + attend_cat:time + BMI_adjust + (1 | subj),
+  data = PTH_delta_data
+)
+
+# R-squared
+rsquared(delta_PTH_LMM)
+
+# Fixed effects test
+anova(delta_PTH_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(delta_PTH_LMM)
+
+# Estimated marginal means for group
+group_delta_TH_emm <- emmeans(delta_PTH_LMM, ~ attend_cat)
+
+# Estimated marginal means for time
+time_delta_TH_emm <- emmeans(delta_PTH_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_delta_PTH_emm  <- emmeans(delta_PTH_LMM, ~ attend_cat:time)
+
+# Post hocs
+ph_delta_PTH_none <- pairs(interaction_delta_PTH_emm, adjust = "none")
+ph_delta_PTH_bonf <- bonferroni(as.data.frame(ph_delta_PTH_none), 3)
+
+# ** delta_vitD -----------------------------------------------------------
+
+delta_vitD_LMM <- lmer(
+  formula = delta_vitD ~ 1 + attend_cat + time + attend_cat:time + BMI_adjust + (1 | subj),
+  data = vitD_delta_data
+)
+
+# R-squared
+rsquared(delta_vitD_LMM)
+
+# Fixed effects test
+anova(delta_vitD_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(delta_vitD_LMM)
+
+# Estimated marginal means for group
+group_delta_TH_emm <- emmeans(delta_vitD_LMM, ~ attend_cat)
+
+# Estimated marginal means for time
+time_delta_TH_emm <- emmeans(delta_vitD_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_delta_vitD_emm  <- emmeans(delta_vitD_LMM, ~ attend_cat:time)
+
+# Post hocs
+ph_delta_vitD_none <- pairs(interaction_delta_vitD_emm, adjust = "none")
+ph_delta_vitD_bonf <- bonferroni(as.data.frame(ph_delta_vitD_none), 3)
+
+# ** delta_sclerostin -----------------------------------------------------
+
+delta_sclerostin_LMM <- lmer(
+  formula = delta_sclerostin ~ 1 + attend_cat + time + attend_cat:time + BMI_adjust + (1 | subj),
+  data = sclerostin_delta_data
+)
+
+# R-squared
+rsquared(delta_sclerostin_LMM)
+
+# Fixed effects test
+anova(delta_sclerostin_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(delta_sclerostin_LMM)
+
+# Estimated marginal means for group
+group_delta_TH_emm <- emmeans(delta_sclerostin_LMM, ~ attend_cat)
+
+# Estimated marginal means for time
+time_delta_TH_emm <- emmeans(delta_sclerostin_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_delta_sclerostin_emm  <- emmeans(delta_sclerostin_LMM, ~ attend_cat:time)
+
+# Post hocs
+ph_delta_sclerostin_none <- pairs(interaction_delta_sclerostin_emm, adjust = "none")
+ph_delta_sclerostin_bonf <- bonferroni(as.data.frame(ph_delta_sclerostin_none), 3)
+
+# ** delta_BMSi -----------------------------------------------------------
+
+delta_BMSi_LMM <- lmer(
+  formula = delta_BMSi ~ 1 + attend_cat + time + attend_cat:time + BMI_adjust + (1 | subj),
+  data = BMSi_delta_data
+)
+
+# R-squared
+rsquared(delta_BMSi_LMM)
+
+# Fixed effects test
+anova(delta_BMSi_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(delta_BMSi_LMM)
+
+# Estimated marginal means for group
+group_delta_TH_emm <- emmeans(delta_BMSi_LMM, ~ attend_cat)
+
+# Estimated marginal means for time
+time_delta_TH_emm <- emmeans(delta_BMSi_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_delta_BMSi_emm  <- emmeans(delta_BMSi_LMM, ~ attend_cat:time)
+
+# Post hocs
+ph_delta_BMSi_none <- pairs(interaction_delta_BMSi_emm, adjust = "none")
+ph_delta_BMSi_bonf <- bonferroni(as.data.frame(ph_delta_BMSi_none), 3)
