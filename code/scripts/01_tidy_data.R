7848bc47208c81ec76f6d6ab3f1ad949775f690f
Lucas Veras <lucasdsveras@gmail.com>
2020-06-12 16:40:58 +0100

    Rearrange code and write tidy data into csv files

--- code/scripts/01_tidy_data.R
+++ code/scripts/01_tidy_data.R
@@ -2,17 +2,19 @@
 
 library(here)
 library(tidyverse)
+library(lvmisc)
 
 # Rename variables --------------------------------------------------------
 
-df <- read_csv(here("data", "database.csv"))
+df <- read_csv(here("data", "raw", "database.csv"))
 
-df <- df %>% dplyr::select(
-    subj = Subj, time = Time, group = Group, surgery = Surgery, sex = Sex, age = Age, 
-    menopause = Menopause, pre_diabetes = `Pre-diabetes`, diabetes = Diabetes,
-    thiazides = Thiazides, smoker = Smoker, BMSi, FN_BMD = Neck_BMD,
-    TH_BMD = Hip__BMD, LS_BMD = LS__BMD, TR_BMD = One_third__BMD, 
-    whole_body_BMD = Whole_Body__BMD, P1NP, CTX, PTH, vitD = VitaminD, 
+df <- df %>%
+  select(
+    subj = Subj, time = Time, group = Group, surgery = Surgery, sex = Sex, 
+    age = Age, menopause = Menopause, pre_diabetes = `Pre-diabetes`, 
+    diabetes = Diabetes, thiazides = Thiazides, smoker = Smoker, BMSi, 
+    FN_BMD = Neck_BMD, TH_BMD = Hip__BMD, LS_BMD = LS__BMD, 
+    TR_BMD = One_third__BMD, P1NP, CTX, PTH, vitD = VitaminD,
     sclerostin = Sclerostin, body_mass = Weight, BMI, 
     whole_body_fat_mass = Whole_Body__Fat_mass, 
     whole_body_lean_mass = Whole_Body__Lean_mass,
@@ -21,12 +23,14 @@ df <- df %>% dplyr::select(
     peak_torque_knee_fle_60ds = PT__Knee__60DS__Flexi,
     peak_torque_knee_ext_60ds_body_mass = PT__Knee__60DS__Exten__Divided__Whole_Body__Total_mass,
     peak_torque_knee_fle_60ds_body_mass = PT__Knee__60DS__Flexi__Divided__Whole_Body__Total_mass,
-    steps = Steps_day, PAEE, SB_h = SB_hours_per_day, LPA_h = LPA_hours_per_day, 
-    MVPA_min = MVPA_min_per_day, 
-    delta_FN_BMD = Delta__Neck__BMD, delta_TH_BMD = Delta__Hip__BMD, delta_LS_BMD = Delta__LS__BMD,
-    delta_TR_BMD = Delta__One_third__BMD, delta_whole_body_BMD = Delta__Whole_Body__BMD,
-    delta_BMSi = Delta__BMSi, delta_P1NP = Delta__P1NP, delta_CTX = Delta__CTX, 
-    delta_PTH = Delta__PTH, delta_vitD = Delta__VitaminaD, delta_sclerostin = Delta__Sclerostin,
+    steps = Steps_day, PAEE, SB_h = SB_hours_per_day, 
+    LPA_h = LPA_hours_per_day, MVPA_min = MVPA_min_per_day, 
+    delta_FN_BMD = Delta__Neck__BMD, delta_TH_BMD = Delta__Hip__BMD, 
+    delta_LS_BMD = Delta__LS__BMD, delta_TR_BMD = Delta__One_third__BMD, 
+    delta_whole_body_BMD = Delta__Whole_Body__BMD, delta_BMSi = Delta__BMSi, 
+    delta_P1NP = Delta__P1NP, delta_CTX = Delta__CTX, 
+    delta_PTH = Delta__PTH, delta_vitD = Delta__VitaminaD, 
+    delta_sclerostin = Delta__Sclerostin,
     delta_body_mass = Delta__Weight, delta_BMI = Delta__BMI, 
     delta_whole_body_fat_mass = Delta__Whole_Body__Fat_mass, 
     delta_whole_body_lean_mass = Delta__Whole_Body__Lean_mass, 
@@ -43,39 +47,49 @@ df <- df %>% dplyr::select(
     age_adjust = Age_adjust, body_mass_adjust = Weight_adjust, BMI_adjust, BMSi_adjust, 
     FN_BMD_adjust = Neck_BMD_adjust, TH_BMD_adjust = Hip__BMD_adjust, 
     LS_BMD_adjust = LS__BMD_adjust, TR_BMD_adjust = One_third__BMD_adjust,
-    whole_body_MBD_adjust = Whole_Body__BMD_adjust, P1NP_adjust, CTX_adjust, PTH_adjust, 
-    vitD_adjust = VitaminD_adjust, sclerostin_adjust = Sclerostin_adjust, BMSi_Cat,
-    time_continuous = Continuous_Time, time_continuous_0123 = Continuous_Time_0123
+    whole_body_BMD_adjust = Whole_Body__BMD_adjust, P1NP_adjust, CTX_adjust, PTH_adjust, 
+    vitD_adjust = VitaminD_adjust, sclerostin_adjust = Sclerostin_adjust, BMSi_Cat
+  ) %>% 
+  mutate(
+    time = as.factor(time),
+    group = recode(as.factor(group), "0" = "Control", "1" = "Exercise"),
+    surgery = recode(as.factor(surgery), "0" = "RYGB", "1" = "Sleeve"),
+    sex = recode(as.factor(sex), "0" = "Female", "1" = "Male"),
+    menopause = recode(as.factor(menopause), "0" = "No", "1" = "Yes"),
+    pre_diabetes = recode(as.factor(pre_diabetes), "0" = "No", "1" = "Yes"),
+    diabetes = recode(as.factor(diabetes), "0" = "No", "1" = "Yes"),
+    thiazides = recode(as.factor(thiazides), "0" = "No", "1" = "Yes"),
+    smoker = recode(as.factor(smoker), "0" = "No", "1" = "Yes"),
+    attend_cat = recode(
+      as.factor(attend_cat),
+      "0" = "Control",
+      "1" = "Under 50% training attendance",
+      "2" = "Over 50% training attendance"
+    )
+  )
+  
+# Prepare data frame for baseline comparisons -----------------------------
+
+baseline_df <- df %>% filter(time == 1)
+
+df_wide <- read_csv(here("data", "raw", "Database__Wide_format.csv"))
+df_wide <- df_wide %>%
+  select(
+    subj = ID, group = Group, height = Height, 
+    waist_circunference = Waist_circunference__1st,
+    hip_circunference = Hip_circunference__1st, 
+    waist_hip_ratio = Waist_to_Hip_ratio
+  ) %>%
+  mutate(
+    group = recode(as.factor(group), "0" = "Control", "1" = "Exercise")
   )
 
-# Recode variables --------------------------------------------------------
+baseline_df <- baseline_df %>% 
+  full_join(
+    df_wide, by = c("subj", "group")
+  )
 
-df$time <- as.factor(df$time)
-df$group <- as.factor(df$group)
-df$group <- recode(df$group, "0" = "Control", "1" = "Exercise")
-df$surgery <- as.factor(df$surgery)
-df$surgery <- recode(df$surgery, "0" = "RYGB", "1" = "Sleeve")
-df$sex <- as.factor(df$sex)
-df$sex <- recode(df$sex, "0" = "Female", "1" = "Male")
-df$menopause <- as.factor(df$menopause)
-df$menopause <- recode(df$menopause, "0" = "No", "1" = "Yes")
-df$pre_diabetes <- as.factor(df$pre_diabetes)
-df$pre_diabetes <- recode(df$pre_diabetes, "0" = "No", "1" = "Yes")
-df$diabetes <- as.factor(df$diabetes)
-df$diabetes <- recode(df$diabetes, "0" = "No", "1" = "Yes")
-df$thiazides <- as.factor(df$thiazides)
-df$thiazides <- recode(df$thiazides, "0" = "No", "1" = "Yes")
-df$smoker <- as.factor(df$smoker)
-df$smoker <- recode(df$smoker, "0" = "No", "1" = "Yes")
-df$attend_cat <- as.factor(df$attend_cat)
-df$attend_cat <- recode(
-  df$attend_cat,
-  "0" = "Control",
-  "1" = "Under 50% training attendance",
-  "2" = "Over 50% training attendance"
-)
-
-# Create a filter ---------------------------------------------------------
+# Exclude subjects --------------------------------------------------------
 
 # Subjects to exclude due to not having at least one follow-up assessment
 exclude <- c(
@@ -83,45 +97,14 @@ exclude <- c(
   53, 57, 59, 64, 65, 66, 74, 76, 82, 84, 86
 )
 
-df$exclude <- NA
-for (i in 1:nrow(df)) {
-  if (df$subj[i] %in% exclude) {
-    df$exclude[i] <- "Yes"
-  } else {
-    df$exclude[i] <- "No"
-  }
-}
-
-# Prepare data frame for baseline comparisons -----------------------------
-
-baseline_df <- df %>% filter(time == 1)
-
-df_wide <- read_csv(here("data", "Database__Wide_format.csv"))
-df_wide <- df_wide %>%
-  dplyr::select(
-    subj = ID, group = Group,height = Height, 
-    waist_circunference = Waist_circunference__1st,
-    hip_circunference = Hip_circunference__1st, 
-    waist_hip_ratio = Waist_to_Hip_ratio
-  )
-df_wide$group <- as.factor(df_wide$group)
-df_wide$group <- recode(df_wide$group, "0" = "Control", "1" = "Exercise")
+df <- df %>% filter(subj %!in% exclude)
 
 baseline_df <- baseline_df %>% 
-  full_join(
-    df_wide, by = c("subj", "group")
+  mutate(
+    exclude = ifelse(subj %in% exclude, "Yes", "No")
   )
 
-baseline_df$exclude <- NA
-for (i in 1:nrow(baseline_df)) {
-  if (baseline_df$subj[i] %in% exclude) {
-    baseline_df$exclude[i] <- "Yes"
-  } else {
-    baseline_df$exclude[i] <- "No"
-  }
-}
+# Write data --------------------------------------------------------------
 
-# Filter subjects out -----------------------------------------------------
-
-df <- df %>% filter(exclude == "No")
-baseline_df <- baseline_df %>% filter(exclude == "No")
+write_csv(df, here("data", "df.csv"))
+write_csv(baseline_df, here("data", "baseline_df.csv"))
