87c87a3fe58f8474edc8cafbb1c174ef412dd3f0
Lucas Veras <lucasdsveras@gmail.com>
2020-06-17 12:16:06 +0100

    Read tidy data and get all model parameters

--- code/scripts/model_acc.R
+++ code/scripts/model_acc.R
@@ -6,62 +6,49 @@ library(lme4)
 library(lmerTest)
 library(piecewiseSEM)
 library(emmeans)
-source(here("code", "functions", "read_data.R"))
-source(here("code", "functions", "center_variable.R"))
-source(here("code", "functions", "bonferroni.R"))
-source(here("code", "functions", "repeat_baseline_values.R"))
 
 # Load and prepare data ---------------------------------------------------
 
-df <- read_data(here("data", "df.csv")) %>% select(subj, time, group, BMI_adjust)
+acc <- read_csv(
+  here("data", "acc.csv"),
+  col_types = cols(
+    time = col_factor(1:4),
+    group = col_factor(c("Control", "Exercise"))
+  )
+)
 
-acc <- read_csv(here("data", "raw", "summary_GRF_data.csv")) %>%
-  dplyr::select(
-    subj = ID, time = eval, duration, n_days, n_peaks, above_thrsh = n_g_4.9
-  ) %>% 
-  mutate(
-    time = recode(
-      as.factor(time),
-      "1st" = "1", "2nd" = "2", "3rd" = "3", "4th" = "4"
-    ),
-    above_thrsh_adjust = repeat_baseline_values(
-      acc, above_thrsh, subj, time, 1
-    )
-  ) %>% 
-  left_join(df, by = c("subj", "time")) %>% 
-  dplyr::select(subj, time, group, everything()) %>% 
-  arrange(subj)
+# Set contrasts of variable group to sum
+contrasts(acc$group) <- matrix(rev(contr.sum(2)), ncol = 1)
+# Set contrasts of variable time to polynomial
+contrasts(acc$time) <- contr.poly(4)
 
 # Model -------------------------------------------------------------------
 
-m <- lmer(
+peaks_LMM <- lmer(
   formula = above_thrsh ~ 1 + group + time + group:time + above_thrsh_adjust + BMI_adjust + (1 | subj),
   data = acc
 )
 
-anova(m, type = 3, test = "F")
+# R-squared
+rsquared(peaks_LMM)
 
-interaction_emm  <- emmeans(m, ~ group:time)
-interaction_emm_df <- as.data.frame(interaction_emm)
+# Fixed effects test
+anova(peaks_LMM, type = 3, test = "F")
 
-pairs(interaction_emm, adjust = "none")
+# Random components and fixed effects parameters estimates
+summary(peaks_LMM)
 
-# Plot --------------------------------------------------------------------
+# Estimated marginal means for group
+group_peaks_emm <- emmeans(peaks_LMM, ~ group)
 
-ggplot(data = interaction_emm_df) +
-  geom_point(
-    aes(x = time, y = emmean, shape = group, colour = group),
-    position = position_dodge(0.2), size = 4
-  ) +
-  geom_line(
-    aes(x = time, y = emmean, linetype = group, group = group, colour = group),
-    position = position_dodge(0.2), size = 1
-  ) +
-  geom_errorbar(
-    aes(x = time, ymin = lower.CL, ymax = upper.CL, group = group, colour = group),
-    position = position_dodge(0.2), size = 1, width = 0.1
-  ) +
-  labs(
-    x = "time",
-    y = "n peaks > 4.9 g"
-  )
+# Estimated marginal means for time
+time_peaks_emm <- emmeans(peaks_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_peaks_emm  <- emmeans(peaks_LMM, ~ group:time)
+# Save into a data frame to build the plots
+interaction_peaks_emm_df <- as.data.frame(interaction_peaks_emm)
+write_csv(interaction_peaks_emm_df, here("output", "interaction_peaks_emm.csv"))
+
+# Post hoc
+pairs(interaction_peaks_emm, adjust = "none")
