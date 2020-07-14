f0bd03d42f973d9801a6f82e68c1ffe904fe5d80
Lucas Veras <lucasdsveras@gmail.com>
2020-07-14 18:48:01 +0100

    Change how the confidence intervals are calculated

--- code/functions/mean_difference.R
+++ code/functions/mean_difference.R
@@ -1,44 +1,35 @@
-mean_difference <- function(data, emm) {
+mean_difference <- function(data) {
   # Compute the difference and 95% confidence interval between two group means
   #
   # Args:
-  #   data: A data frame
-  #   emm: The estimated marginal means as a data frame
+  #   data: A emmGrid object with the contrasts
   #
   # Returns:
-  #   A data frame with the mean difference, lower and upper confidence interval
-  #   values
+  #   A data frame with the mean difference, lower and upper confidence
+  # interval and p value
   
-  # Get sample sizes
-  n <- data %>% 
-    filter(time == 4) %>% 
-    select(subj, group, ends_with("_BMD")) %>% 
-    na.omit() %>% 
-    group_by(group) %>% 
-    summarise(n = n(), .groups = "drop")
-  
-  n1 <- as.double(n[which(n$group == "Exercise"), 2])
-  n2 <- as.double(n[which(n$group == "Control"), 2])
-  
-  # Get mean difference
-  emm1 <- emm[which(emm$group == "Exercise" & emm$time == 4), 3]
-  emm2 <- emm[which(emm$group == "Control" & emm$time == 4), 3]
-  mean_diff <- emm1 - emm2
-  
-  # Get standard deviations
-  sd1 <- emm[which(emm$group == "Exercise" & emm$time == 4), 4] * sqrt(n1)
-  sd2 <- emm[which(emm$group == "Control" & emm$time == 4), 4] * sqrt(n1)
-  
-  # Get pooled standard deviation
-  sd_pooled <- sqrt(((n1 - 1) * sd1 + (n2 - 1) * sd2) / (n1 + n2 - 2)) 
-  
-  # Get pooled standard error
-  se_pooled <- sd_pooled * sqrt((1 / n1) + (1 / n2))
-  
-  # Get confidence interval
-  lower_ci <- mean_diff - se_pooled * qt(0.975, df = n1 + n2 - 2)
-  upper_ci <- mean_diff + se_pooled * qt(0.975, df = n1 + n2 - 2)
-  
-  # Return the mean difference and confidence interval
-  data.frame(mean_diff, lower_ci, upper_ci)
+  p_value <- round(as.data.frame(data)[28, 6], 3)
+  mean_diff <- round(- as.data.frame(data)[28, 2], 3)
+  se <- round(as.data.frame(data)[28, 3], 3)
+  lower_ci <- round(mean_diff - 1.96 * se, 3)
+  upper_ci <- round(mean_diff + 1.96 * se, 3)
+  data.frame(mean_diff, lower_ci, upper_ci, p_value)
+}
+
+mean_difference_delta <- function(data) {
+  p_value <- round(as.data.frame(data)[65, 6] * 3, 3)
+  mean_diff <- round(- as.data.frame(data)[65, 2], 1)
+  se <- round(as.data.frame(data)[65, 3], 1)
+  lower_ci <- round(mean_diff - 1.96 * se, 1)
+  upper_ci <- round(mean_diff + 1.96 * se, 1)
+  data.frame(mean_diff, lower_ci, upper_ci, p_value)
+}
+
+mean_difference_delta_2 <- function(data) {
+  p_value <- round(as.data.frame(data)[66, 6] * 3, 3)
+  mean_diff <- round(- as.data.frame(data)[66, 2], 1)
+  se <- round(as.data.frame(data)[66, 3], 1)
+  lower_ci <- round(mean_diff - 1.96 * se, 1)
+  upper_ci <- round(mean_diff + 1.96 * se, 1)
+  data.frame(mean_diff, lower_ci, upper_ci, p_value)
 }
