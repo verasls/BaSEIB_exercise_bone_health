f62a7f13d33dd09498e52fd3a4ddf014fc8fa4e2
Lucas Veras <lucasdsveras@gmail.com>
2020-07-29 20:18:59 +0100

    Model number of acceleration peaks above 4.9g threshold

--- code/scripts/05_pri_anal_model_BC_PA_strength_vars.R
+++ code/scripts/05_pri_anal_model_BC_PA_strength_vars.R
@@ -12,10 +12,26 @@ source(here("code", "functions", "mean_difference.R"))
 # Load and prepare data ---------------------------------------------------
 
 df <- read_data(here("data", "df.csv"))
+acc_peaks <- read_csv(
+  here("data", "acc.csv"),
+  col_types = cols(
+    time = col_factor(1:4),
+    group = col_factor(c("Control", "Exercise"))
+  )
+) %>% 
+  select(- BMI_adjust) %>% 
+  left_join(df, by = c("subj", "time", "group")) %>% 
+  select(
+    subj, time, group, above_thrsh, above_thrsh_adjust , BMI_adjust, 
+    surgery, age, menopause, diabetes, thiazides, smoker
+  )
+
 # Set contrasts of variable group to sum
 contrasts(df$group) <- matrix(rev(contr.sum(2)), ncol = 1)
+contrasts(acc_peaks$group) <- matrix(rev(contr.sum(2)), ncol = 1)
 # Set contrasts of variable time to polynomial
 contrasts(df$time) <- contr.poly(4)
+contrasts(acc_peaks$time) <- contr.poly(4)
 
 # Select variables
 # Body composition
@@ -333,6 +349,34 @@ write_csv(interaction_MVPA_emm_df, here("output", "interaction_MVPA_emm.csv"))
 
 # Post hocs
 ph_MVPA_none <- pairs(interaction_MVPA_emm, adjust = "none")
+
+# **** Number of peaks ----------------------------------------------------
+
+peaks_LMM <- lmer(formula = build_formula("above_thrsh"), data = acc_peaks)
+
+# R-squared
+rsquared(peaks_LMM)
+
+# Fixed effects test
+anova(peaks_LMM, type = 3, test = "F")
+
+# Random components and fixed effects parameters estimates
+summary(peaks_LMM)
+
+# Estimated marginal means for group
+group_peaks_emm <- emmeans(peaks_LMM, ~ group)
+
+# Estimated marginal means for time
+time_peaks_emm <- emmeans(peaks_LMM, ~ time)
+
+# Estimated marginal means for group x time interaction
+interaction_peaks_emm  <- emmeans(peaks_LMM, ~ group:time)
+# Save into a data frame to build the plots
+interaction_peaks_emm_df <- as.data.frame(interaction_peaks_emm)
+write_csv(interaction_peaks_emm_df, here("output", "interaction_peaks_emm.csv"))
+
+# Post hocs
+ph_peaks_none <- pairs(interaction_peaks_emm, adjust = "none")
 
 # ** Isokinetic strength variables ----------------------------------------
 
